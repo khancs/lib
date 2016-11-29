95e0264c0cfc8c6b53cf501d097527d1406fd669
khancs <khanacademycs+github@gmail.com>
2016-11-29 15:13:48 -0500

    Add basic functionality to list.js

--- list.js
+++ list.js
@@ -2,3 +2,18 @@
   lib/list.js 
   Originally created on 11/29/16 by KhanCS (https://github.com/khancs).
 */
+
+(function(context) {
+    if (typeof context.ka === 'undefined') {
+        context.ka = {};
+    }
+    var ka = context.ka;
+    ka._libRoot = "https://cdn.rawgit.com/khancs/lib/master/.../lib/";
+    ka.libraries = {
+        "pjs": {
+            name: "Khan Academy Processing.js",
+            desc: "A library to produce environments to emulate the Khan Academy Processing.js environment",
+            src: "khancs/pjs.js"
+        }
+    };
+})(this);
